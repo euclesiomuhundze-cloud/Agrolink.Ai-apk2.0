f1032e44c3106b77d58b3d4e4170c4dc97e738fd
Euclesio <Euclesiomuhundze@gmail.com>
2026-07-15 19:28:49 +0200

    Adicionado mapeamento global do plugin do Jetpack Compose

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,15 @@
 // Ficheiro de configuração global da raiz compatível com Gradle 9+
 plugins {
-    // Define a versão do Plugin do Android (sem aplicar diretamente na raiz)
+    // Define a versão do Plugin do Android
     id("com.android.application") version "8.2.2" apply false
 
-    // Define a versão do Kotlin para Android (sem aplicar diretamente na raiz)
+    // Define a versão do Kotlin para Android
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
-    // Se usares Firebase/Google Services, define aqui também:
+    // Define a versão do plugin do Jetpack Compose compiler
+    id("org.jetbrains.kotlin.plugin.compose") version "1.9.22" apply false
+
+    // Se usares Firebase/Google Services:
     id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
