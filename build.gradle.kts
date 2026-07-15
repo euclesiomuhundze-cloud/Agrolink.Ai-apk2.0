4d5390bc4222a85eb0b838b7e4d00469363cb32a
Euclesio <Euclesiomuhundze@gmail.com>
2026-07-15 18:46:51 +0200

    Corrigido build.gradle.kts da raiz para sintaxe classica compativel

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,20 @@
-// Ficheiro de configuração global do projeto
-plugins {
-    alias(libs.plugins.android.application) apply false
-    alias(libs.plugins.kotlin.android) apply false
+// Ficheiro de configuração global clássico da raiz
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        // Define as classes do plugin do Android e do Kotlin globalmente
+        classpath("com.android.tools.build:gradle:8.2.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
 }
 
