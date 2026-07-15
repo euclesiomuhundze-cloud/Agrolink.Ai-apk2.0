8b86e1e39d35a88116a6debb862c3a4c68fceaf4
Euclesio <Euclesiomuhundze@gmail.com>
2026-07-15 19:04:13 +0200

    Definicao centralizada de versões de plugins para compatibilidade com Gradle 9

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,12 @@
-// Ficheiro de configuração global clássico da raiz
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        // Define as classes do plugin do Android e do Kotlin globalmente
-        classpath("com.android.tools.build:gradle:8.2.2")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
-    }
+// Ficheiro de configuração global da raiz compatível com Gradle 9+
+plugins {
+    // Define a versão do Plugin do Android (sem aplicar diretamente na raiz)
+    id("com.android.application") version "8.2.2" apply false
+
+    // Define a versão do Kotlin para Android (sem aplicar diretamente na raiz)
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    // Se usares Firebase/Google Services, define aqui também:
+    id("com.google.gms.google-services") version "4.4.1" apply false
 }
 
