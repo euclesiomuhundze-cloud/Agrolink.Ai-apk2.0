345f76c7524c45d11ead3a8c3cfe1e1ed285f310
Euclesio <Euclesiomuhundze@gmail.com>
2026-07-15 19:51:49 +0200

    Corrige build.gradle.kts com bloco plugins ausente

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,11 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("org.jetbrains.kotlin.plugin.compose")
+    id("org.jetbrains.kotlin.plugin.serialization")
+    id("com.google.gms.google-services")
 }
+
 
 android {
     namespace = "com.agon.app"
